d30ca6bcc632914e954ad0172c29907374b3aefd
HenryHitak <gmlxkr920901@gmail.com>
2022-08-02 21:00:35 -0700

    starting vue app

---
+++ vue/vue/vue_demo/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <my-comp name='Henry' family='Lee'></my-comp>
+  </div>
+</template>
+
+<script>
+import myComp from './components/MyComp.vue'
+export default {
+  name: 'App',
+  components: {
+    myComp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/vue/vue_demo/src/components/MyComp.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>My name is {{name}} {{family}}</h1>
+    </div>
+</template>
+<script>
+export default {
+    name:"myCompo",
+    props:['name','family']
+}
+</script>
+<style> 
+h1{
+    background-color: black;
+    color: white;
+}
+</style>
